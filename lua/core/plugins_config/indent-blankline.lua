4dd4a080369a26680beccd7396d418782c5b5059
Tomas Burianek <tomas.burianek@kiwi.com>
2024-08-05 10:17:51 +0200

    feat(plugins): Make indent blankline less annoying

--- lua/core/plugins_config/indent-blankline.lua
+++ lua/core/plugins_config/indent-blankline.lua
@@ -1,8 +1,7 @@
 require("ibl").setup({
-    indent = { highlight = { "CursorColumn" }, char = "▏", priority = 0 },
+    indent = { highlight = { "Comment" }, char = "▏", priority = 0 },
     whitespace = { highlight = { "Comment" } },
     scope = {
-        enabled = true,
-        show_end = true
+        enabled = false,
     }
 })
